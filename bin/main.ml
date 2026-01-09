d2d6c1797332350f2346af81e69bc974d7c71f82
Jan Hoffmann <uni@hoffjan.de>
2026-01-09 00:22:41 -0500

    pretty printer for values

--- bin/main.ml
+++ bin/main.ml
@@ -4,6 +4,9 @@ let () =
   let file_name = (Sys.get_argv ()).(1) in
   let str = In_channel.read_all file_name in
   let exp = Parser.exp str in
-  let _ = Printf.printf "Expression:\n%s\n" (Syntax.Exp.to_string exp) in
+  let () = Printf.printf "Parsing successful.\n" in
   let typ = Statics.type_exp exp in
-  Printf.printf "Type:\n%s\n" (Syntax.Typ.to_string typ)
+  let () = Printf.printf "Type checking successful\n  Typ: %s\n" (Syntax.Typ.to_string typ) in
+  let () = Printf.printf "Starting evaluation ... \n" in
+  let value = Dynamics.Eval.eval exp in
+  Dynamics.Pprint.print_value value
