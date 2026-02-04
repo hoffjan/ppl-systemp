5024156891c1e80b2de74f987bdc4163e27cb8e2
Jan Hoffmann <uni@hoffjan.de>
2026-02-03 23:34:22 -0500

    bugfix: booleans

---
+++ examples/bool.p
@@ -0,0 +1,2 @@
+let t = True {} in
+case 2>3 { True x -> True x | False x -> False x }
--- lib/parser/parser.ml
+++ lib/parser/parser.ml
@@ -14,10 +14,13 @@ let typ ?(typ_of_str = fun _ -> failwith "No type variable function provided") s
 
 (* --- Types --- *)
 let exp str =
+  let module T = Syntax.Typ in
+  let consts =
+    match T.out T.bool with T.Tsum bconst -> Map.map ~f:(fun _ -> bconst) bconst | _ -> failwith "bool is a sum type"
+  in
   (* parse_string takes: parser -> input -> user_state -> result *)
   match
-    parse_string (Exp.parse << eof) str
-      { types = String.Map.singleton "bool" Syntax.Typ.bool; env = String.Map.empty; consts = Syntax.Label.Map.empty }
+    parse_string (Exp.parse << eof) str { types = String.Map.singleton "bool" T.bool; env = String.Map.empty; consts }
   with
   | Success t -> t
   | Failed (msg, _) -> exit_with msg
